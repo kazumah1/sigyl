d5157d0ceea8e790d2eba7b422ccac1c509d3a56
Kazuma <162946059+kazumah1@users.noreply.github.com>
2025-06-27 03:28:39 -0700

    js-basic

---
+++ examples/generated-mcps/js-basic-2025-06-27T10-21-02/tools/postApiUsers.js
@@ -0,0 +1,5 @@
+export async function postApiUsers(args) {
+				// TODO: Implement actual logic for POST /api/users
+				return { content: [ { type: "text", text: "Dummy response from postApiUsers" } ] };
+			}
+			
