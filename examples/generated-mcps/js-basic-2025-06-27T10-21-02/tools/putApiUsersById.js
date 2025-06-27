1adc3916a4775f4873bdfac82746b50dc4a83536
Kazuma <162946059+kazumah1@users.noreply.github.com>
2025-06-27 03:32:00 -0700

    js-with-query

--- examples/generated-mcps/js-basic-2025-06-27T10-21-02/tools/putApiUsersById.js
+++
@@ -1,5 +0,0 @@
-export async function putApiUsersById(args) {
-				// TODO: Implement actual logic for PUT /api/users/:id
-				return { content: [ { type: "text", text: "Dummy response from putApiUsersById" } ] };
-			}
-			
---
+++ examples/generated-mcps/js-query-2025-06-27T10-30-35/tools/getApiProducts.js
@@ -0,0 +1,5 @@
+export async function getApiProducts(args) {
+				// TODO: Implement actual logic for GET /api/products
+				return { content: [ { type: "text", text: "Dummy response from getApiProducts" } ] };
+			}
+			
