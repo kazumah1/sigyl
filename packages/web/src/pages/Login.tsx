02ca898cee4e0d82d0124f7e8375639ee2bce021
1CharlieMartin <ckm91011@gmail.com>
2025-07-02 15:04:19 -0700

    gh login redirect fix for prod

--- packages/web/src/pages/Login.tsx
+++ packages/web/src/pages/Login.tsx
@@ -30,7 +30,7 @@ const Login = () => {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'github',
         options: {
-          redirectTo: 'http://localhost:8080/auth/callback',
+          redirectTo: window.location.origin + '/auth/callback',
           scopes: 'read:user user:email'
         }
       });
